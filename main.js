dc3d60d3b4478dbb3ee4595023d6ee2a30938a1a
gift-a <ksenia.gift@gmail.com>
2018-02-26 10:37:07 +0200

    realise prototype style. it worksgit add .

--- main.js
+++ main.js
@@ -1,181 +1,183 @@
 window.addEventListener("load", function() {
   var myToDoList = new ToDoList();
-  console.log(myToDoList.getName());
-  console.dir(myToDoList.getToDoBox());
+  myToDoList.generateToDoList("My list");
 });
 
-function ToDoList(name) {
-  this._name = name || "Your To Do List";
-  this._todoBox = this._generateToDoList(this._name);
+function ToDoList() {
+  DOMElem.call(this);
 }
-
-ToDoList.prototype.getName = function() {
-  return this._name;
-};
-
-ToDoList.prototype.getToDoBox = function() {
-  return this._todoBox;
-};
-
-ToDoList.prototype._generateToDoList = function(name) {
+ToDoList.prototype = Object.create(DOMElem.prototype);
+ToDoList.prototype.constructor = ToDoList;
+ToDoList.prototype.generateToDoList = function(name) {
   //create box and inner elements
-  var todoBox = this._createDOMElement("div", { class: "todo-container" }),
-    textInput = this._createDOMElement("input", {
-      class: "text",
-      type: "text",
-      placeholder: "type your to do and click Create or press Enter"
-    }),
-    btnCreate = this._createDOMElement("input", {
-      class: "button button_create",
-      type: "button",
-      value: "Create",
-      name: "create"
-    }),
-    btnDelAll = this._createDOMElement("input", {
-      class: "button button_del-all",
-      type: "button",
-      value: "Delete all",
-      name: "delAll"
-    }),
-    header = this._createDOMElement("h1", { class: "header" });
+  var name = name || "Your To Do List";
+  var todoBox = this.createElem("div", { class: "todo-container" });
+  var inner = this.createElems([
+    { h1: { class: "header" } },
+    {
+      input: {
+        class: "text",
+        type: "text",
+        placeholder: "type your to do and click Create or press Enter"
+      }
+    },
+    {
+      input: {
+        class: "button button_create",
+        type: "button",
+        value: "Create",
+        name: "create"
+      }
+    },
+    {
+      input: {
+        class: "button button_del-all",
+        type: "button",
+        value: "Delete all",
+        name: "delAll"
+      }
+    }
+  ]);
 
-  header.innerHTML = name || "Your To Do List";
+  inner[0].innerHTML = name;
 
   //add elements in document
-  this._appendChildren(todoBox, [header, textInput, btnCreate, btnDelAll]);
+  this.addElems(todoBox, inner);
   document.body.appendChild(todoBox);
 
   //add handlers for buttons "create" and "delete all"
-  btnCreate.addEventListener("click", function(event) {
-    ToDoList.prototype._addListItem(todoBox);
-  });
 
-  textInput.addEventListener("keydown", function(event) {
+  inner[1].addEventListener("keydown", function(event) {
     if (event.keyCode == "13") {
-      ToDoList.prototype._addListItem(todoBox);
+      var listItem = new ListItem();
+      listItem.addListItem(todoBox, event.target);
     }
   });
+  inner[2].addEventListener("click", function(event) {
+    var textHandler = todoBox.querySelector("[type=text]");
+    var listItem = new ListItem();
+    listItem.addListItem(todoBox, textHandler);
+  });
 
-  btnDelAll.addEventListener("click", function(event) {
-    ToDoList.prototype._deleteAllItems(todoBox);
+  inner[3].addEventListener("click", function(event) {
+    var list = todoBox.querySelector(".list");
+    ToDoList.prototype.delElem(list);
   });
 
   return todoBox;
 };
 
-ToDoList.prototype._deleteAllItems = function(todoBox) {
-  var list = todoBox.querySelector(".list");
-  todoBox.removeChild(list);
-};
-
-ToDoList.prototype._addListItem = function(parent, children, valueHandler) {
-  //cearch list and input data
-  var list = todoBox.querySelector(".list"),
-    input = todoBox.querySelector("[type=text]"),
-    inputText = input.value;
-  //prevent empty items
-  if (!inputText) return;
-  listItem = this._createListItem(inputText);
-  //create list if it does not exsist
-  if (!list) {
-    list = this._createList();
-    todoBox.insertBefore(list, todoBox.querySelector("[name=delAll]"));
-  }
-  list.appendChild(listItem);
-  //clean input field
-  input.value = "";
-};
+// ******** UL with onklick handlers on Done and Del **********
 
 function List() {
-  Entity.call(this);
+  DOMElem.call(this);
   //create empty list
-  this._list = this.createDOMElement("ul", { class: "list" });
+}
+List.prototype = Object.create(DOMElem.prototype);
+List.prototype.constructor = List;
+List.prototype.createList = function() {
+  var list = this.createElem("ul", { class: "list" });
   //add handler for clicks on list items
-  this._list.addEventListener("click", function(event) {
+  list.addEventListener("click", function(event) {
     var target = event.target;
     if (target.tagName != "INPUT") return;
     if (target.getAttribute("name") == "done") {
-      List.prototype._markItemDone(target.parentElement);
+      List.prototype.markItemDone(target.parentElement);
     } else if (target.getAttribute("name") == "del") {
-      List.prototype._delItem(target.parentElement);
+      List.prototype.delElem(target.parentElement);
     }
   });
-  return this._list;
-}
-List.prototype = Object.create(Entity.prototype);
-List.prototype.constructor = ListItem;
-List.prototype._markItemDone = function(listItem) {
+  return list;
+};
+List.prototype.markItemDone = function(listItem) {
   listItem.classList.add("list__item_done");
 };
-List.prototype._delItem = function(listItem) {
-  listItem.parentElement.removeChild(listItem);
+
+// ******** LI with buttons Done and Del **********
+
+function ListItem(text) {
+  DOMElem.call(this);
+}
+ListItem.prototype = Object.create(DOMElem.prototype);
+ListItem.prototype.constructor = ListItem;
+ListItem.prototype.addListItem = function(container, textHandler) {
+  //cearch list and input data
+  var list = container.querySelector(".list"),
+    text = textHandler.value;
+  //prevent empty items
+  if (!text) return;
+  var listItem = this.createListItem(text);
+  //create list if it does not exsist
+  if (!list) {
+    var ul = new List();
+    list = ul.createList();
+    container.insertBefore(list, container.querySelector("[name=delAll]"));
+  }
+  list.appendChild(listItem);
+  //clean input field
+  textHandler.value = "";
 };
 
-function ListItem() {
-  Entity.call(this);
+ListItem.prototype.createListItem = function(text) {
   //create list item and inner elements
-  this._listItem = this.createDOMElement("li", { class: "list__item" });
-  this._listText = this.createDOMElement("span", { class: "item__text" });
-  this._doneBtn = this._createDOMElement("input", {
-    class: "button button_done",
-    type: "Button",
-    value: "Done!",
-    name: "done"
-  });
-  this._delBtn = this._createDOMElement("input", {
-    class: "button button_del",
-    type: "Button",
-    value: "Delete",
-    name: "del"
-  });
-  this._listText.innerHTML = text;
-  //add inner elements in list item
-  this.createEntity(this._listItem, [
-    this._listText,
-    this._doneBtn,
-    this._delBtn
+  var listItem = this.createElem("li", { class: "list__item" });
+  var inner = this.createElems([
+    { span: { class: "item__text" } },
+    {
+      input: {
+        class: "button button_done",
+        type: "Button",
+        value: "Done!",
+        name: "done"
+      }
+    },
+    {
+      input: {
+        class: "button button_del",
+        type: "Button",
+        value: "Delete",
+        name: "del"
+      }
+    }
   ]);
+  inner[0].innerHTML = text;
+  this.addElems(listItem, inner);
+  return listItem;
+};
 
-  return this._listItem;
-}
-ListItem.prototype = Object.create(Entity.prototype);
-ListItem.prototype.constructor = ListItem;
+// ******** DOM Element with main behavior **********
 
-function Entity() {}
+function DOMElem() {}
 // multiple appendChild
 // children - array of children
-Entity.prototype.createEntity = function(parent, children) {
-  children.forEach(function(child) {
+DOMElem.prototype.addElems = function(parent, childrenArr) {
+  childrenArr.forEach(function(child) {
     parent.appendChild(child);
   });
 };
 
-// atr - object with keys {attributeName: value}
-Entity.prototype.createDOMElement = function(tagName, atr) {
+// input attr: object with keys {attributeName: value}
+DOMElem.prototype.createElem = function(tagName, attr) {
   var elem = document.createElement(tagName);
-  for (var key in atr) {
-    elem.setAttribute(key, atr[key]);
+  for (var key in attr) {
+    elem.setAttribute(key, attr[key]);
   }
   return elem;
 };
-Entity.prototype.addEntity = function(parent, entity, child, valueHandler) {
-  //cearch list and input data
-  //  var list = todoBox.querySelector(".list"),
-  //    input = todoBox.querySelector("[type=text]"),
-  //    inputText = input.value;
 
-  /************* CHECK ABOVE!!! *****************/
-
-  var value = valueHandler.value;
-  //prevent empty items
-  if (!value) return;
-  child = this._createListItem(inputText);
-  //create list if it does not exsist
-  if (!entity) {
-    entity = this.createEntity(parent, entity);
-    todoBox.insertBefore(list, todoBox.querySelector("[name=delAll]"));
+// create multiple
+// input: array type [{tag1:{attrubute: value}}, tag2{...}...]
+// output: array of elements
+DOMElem.prototype.createElems = function(tags) {
+  var elemArr = [];
+  for (var i = 0; i < tags.length; i++) {
+    for (var tag in tags[i]) {
+      elemArr.push(this.createElem(tag, tags[i][tag]));
+    }
   }
-  list.appendChild(listItem);
-  //clean input field
-  input.value = "";
+  return elemArr;
+};
+
+DOMElem.prototype.delElem = function(element) {
+  element.parentElement.removeChild(element);
 };

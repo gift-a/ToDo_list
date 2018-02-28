5fa85eba6b11e152a00b3b010493336f5de76889
gift-a <ksenia.gift@gmail.com>
2018-02-28 10:41:12 +0200

    refactoring task

--- script/task.js
+++ script/task.js
@@ -1,35 +1,48 @@
-function Task(id, description, date) {
-  this._id = id;
-  this._description = description;
-  this._date = date;
-  this._isDone = false;
+class Task {
+  constructor(id, description, date = new Date()) {
+    // сделать статическим???
+    this._id = id;
+    this._description = description;
+    this._date = date;
+    this._isDone = false;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  set description(description) {
+    this._description = description;
+  }
+
+  get date() {
+    return this._date;
+  }
+
+  set date(date) {
+    this._date = date;
+  }
+
+  get isDone() {
+    return this._isDone;
+  }
+  set isDone(value) {
+    this._isDone = value;
+  }
+  toggleDone() {
+    if (!this.isDone) this.isDone = true;
+    else this.isDone = false;
+  }
 }
 
-Task.prototype.getId = function() {
-  return this._id;
-};
-
-Task.prototype.getDescription = function() {
-  return this._description;
-};
-
-Task.prototype.setDescription = function(description) {
-  this._description = description;
-};
-
-Task.prototype.getDate = function() {
-  return this._date;
-};
-
-Task.prototype.setDate = function(date) {
-  this._date = date;
-};
-
-Task.prototype.getIsDone = function() {
-  return this._isDone;
-};
-
-Task.prototype.toggleDone = function() {
-  if (!this._isDone) this._isDone = true;
-  else this._isDone = false;
-};
+let task = new Task(1, "my task");
+console.log(task.id);
+console.log(task.description);
+console.log(task.date);
+console.log(task.isDone);
+task.toggleDone();
+console.log(task.isDone);
